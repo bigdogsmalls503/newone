f25ab9740ff6bd686eb3dabfd4ad41d31816b136
bigdogsmalls503 <andrewmoist503@yahoo.com>
2025-08-10 08:27:27 -0700

    Delete alien-quiz directory

--- alien-quiz/quiz.js
+++
@@ -1,9 +0,0 @@
-
-function saveAnswer(event, key, nextPage) {
-  event.preventDefault();
-  const answer = document.querySelector('input[name="answer"]:checked');
-  if (answer) {
-    sessionStorage.setItem(key, answer.value);
-    window.location.href = nextPage;
-  }
-}
